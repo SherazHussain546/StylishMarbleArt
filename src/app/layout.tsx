3f7d4904c91cee4bd9cb4e1b370de7a5c09dfd05
Sheraz Hussain <sherazhussainofficial1@gmail.com>
2025-10-20 22:28:17 +0000

    add this tagas well please <!-- Google tag (gtag.js) --> <script async

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -112,6 +112,18 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* New Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-K21SD8BH3Z"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-K21SD8BH3Z');
+            `,
+          }}
+        />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
